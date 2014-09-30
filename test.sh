11395ebb7e7c9721742759f4ff681cafb2246ea3
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-09-30 17:01:48 -0300

    Add Travis CI support.

--- test.sh
+++ test.sh
@@ -10,6 +10,8 @@ comp_value=$?  #Look at diff exit status
 if [ $comp_value -eq 1 ]
 then
     echo "WARNING: Output is different!"
+    exit 1;
 else
     echo "SUCCESS: Output is the same!"
+    exit 0;
 fi
