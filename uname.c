1461ebe08cf4ae6b51e3de222cfc5f177c48efa7
Andrew Valencik <andrew.valencik@gmail.com>
2014-09-30 21:01:17 -0300

    test travis...

--- uname.c
+++ uname.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/utsname.h>
 
 int main(int argc, char *argv[]) {
 
-        struct UNAME_STRUCT ubuffer;
+        struct utsname ubuffer;
 
     // Check for proper usage
     if (argc != 1) {
@@ -16,6 +17,10 @@ int main(int argc, char *argv[]) {
     uname(&ubuffer);
 
     printf("System name: %s\n", ubuffer.sysname);
+    printf("Host name: %s\n", ubuffer.nodename);
+    printf("OS Release: %s\n", ubuffer.release);
+    printf("OS Version: %s\n", ubuffer.version);
+    printf("CPU Type: %s\n", ubuffer.machine);
 
     return 0;
 }
