082f1ce46a9eb26bcd4a260c8fdf3f3f62c6bcae
Andrew Valencik <andrew.valencik@gmail.com>
2014-10-01 12:03:54 -0300

    Minimal solution with warning about copying.

--- uname.c
+++ uname.c
@@ -1,26 +1,17 @@
+//Do not copy this code or anyone elses
+//You would only be hurting yourself
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/utsname.h>
-
-int main(int argc, char *argv[]) {
-
-        struct utsname ubuffer;
-
-    // Check for proper usage
-    if (argc != 1) {
-        fprintf(stderr, "%s: Aborting, not enough arguments.\n", argv[0]);
-        return (-1);
-    }
-
+int main() {
+    struct utsname ubuffer;
     uname(&ubuffer);
-
     printf("System name: %s\n", ubuffer.sysname);
     printf("Host name: %s\n", ubuffer.nodename);
     printf("OS Release: %s\n", ubuffer.release);
     printf("OS Version: %s\n", ubuffer.version);
     printf("CPU Type: %s\n", ubuffer.machine);
-
     return 0;
 }
