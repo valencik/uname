a1edee22889291deb9d4ec234cf98f38d81d4baa
Andrew Valencik <andrew.valencik@gmail.com>
2014-10-01 11:46:40 -0300

    Added formatted output for code and system data.
    Used tput for colouring terminal output in test.sh
    (Useful details at http://stackoverflow.com/a/20983251)
    Both test data and code data are saved to text files.
    Added both output textfiles to .gitignore
    [skip ci]

--- test.sh
+++ test.sh
@@ -2,6 +2,12 @@
 #This script takes a single parameter, the path to your uname.c code.
 #Note that compiling will fail if any warnings are present.
 
+#Define message output types
+TEST="$(tput setaf 4)TEST:$(tput sgr 0)"
+ERROR="$(tput setaf 1)ERROR:$(tput sgr 0)"
+SUCCESS="$(tput setaf 2)SUCCESS:$(tput sgr 0)"
+
+
 #Check command line arguments
 if [ "$#" -ne 1 ]; then
     echo "Error: $0 takes one argument, the C code to compile."
@@ -15,27 +21,34 @@ rm testUname 2> /dev/null
 gcc -Wall -Wextra -Werror -Wfatal-errors $1 -o testUname
 gcc_result=$? #save gcc exit status
 if [ $gcc_result -ne 0 ]; then
-    echo "ERROR: GCC failed to compile without warnings!"
+    echo "${ERROR} GCC failed to compile without warnings!"
     exit 1;
+else
+    echo "${TEST} GCC compiled without warnings. Good."
 fi
 
 #Generate test data
-echo "System name: $(uname -s)"  > UnameOutput.txt
-echo "Host name: $(uname -n)"   >> UnameOutput.txt
-echo "OS Release: $(uname -r)"  >> UnameOutput.txt
-echo "OS Version: $(uname -v)"  >> UnameOutput.txt
-echo "CPU Type: $(uname -m)"    >> UnameOutput.txt
+echo "${TEST} Here is the generated system data to match:"
+echo "System name: $(uname -s)" | tee    systemUnameOutput.txt
+echo "Host name: $(uname -n)"   | tee -a systemUnameOutput.txt
+echo "OS Release: $(uname -r)"  | tee -a systemUnameOutput.txt
+echo "OS Version: $(uname -v)"  | tee -a systemUnameOutput.txt
+echo "CPU Type: $(uname -m)"    | tee -a systemUnameOutput.txt
+
+#Log code's output
+echo "${TEST} Here is the output of your code, $1:"
+./testUname | tee testUnameOutput.txt
 
 #Compare output of code to generated test data
-diff --brief <(./testUname) UnameOutput.txt >/dev/null
+diff --brief testUnameOutput.txt systemUnameOutput.txt >/dev/null
 comp_value=$?  #save diff exit status
 
 #Evaluate if code passes or fails.
 if [ $comp_value -eq 1 ]
 then
-    echo "WARNING: Output is different!"
+    echo "${ERROR} Output is different!"
     exit 1;
 else
-    echo "SUCCESS: Output is the same!"
+    echo "${SUCCESS} Output is the same!"
     exit 0;
 fi
